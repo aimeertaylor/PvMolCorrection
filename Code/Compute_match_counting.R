50aa74438a8658422e4805dabc9250bc157b23d6
Aimee Taylor <aimee.r.taylor.85@gmail.com>
2025-06-07 16:17:07 +0200

    Fix match counting algorithm

--- Code/Compute_match_counting.R
+++ Code/Compute_match_counting.R
@@ -6,11 +6,12 @@ n_epi_per_pid <- sapply(ys_VHX_BPD, function(x) length(x))
 
 # Compute matches for  paired data 
 matches_n_p <- sapply(ys_VHX_BPD[n_epi_per_pid > 1], match_counting)
-matches_n <- unlist(sapply(matches_n_p, function(x) x["p",]))
+matches_p <- unlist(sapply(matches_n_p, function(x) x["p",]))
+matches_n <- unlist(sapply(matches_n_p, function(x) x["n",]))
 
 # I first did names(matches_n) <- gsub(".", "_", names(matches_n), fixed = T)
 # Need following because otherwise pids with one episode miss "_2" e.g. BPD_103
-names(matches_n) <- unlist(sapply(names(matches_n_p), function(n) {
+names(matches_p) <- names(matches_n) <- unlist(sapply(names(matches_n_p), function(n) {
   paste(n, colnames(matches_n_p[[n]]), sep = "_")
 }))
 
@@ -19,22 +20,24 @@ load("../RData/marg_results_Pv3Rs.RData")
 
 if (!all(names(matches_n) == row.names(Uniform_Pv3Rs))) stop()
 
-
 # Plot match proportion against 
-plot(x = matches_n, y = Uniform_Pv3Rs[,"L"], 
+plot(x = matches_p, y = Uniform_Pv3Rs[,"L"], 
+     cex = matches_n/9,
      xlab = "Match proportion", 
-     ylab = "Posterior relapse probability (based on genetic data alone)")
+     ylab = "Posterior probability (based on genetic data alone)")
 
-plot(x = matches_n, y = Uniform_Pv3Rs[,"C"], 
+plot(x = matches_p, y = Uniform_Pv3Rs[,"C"], 
+     cex = matches_n/9,
      xlab = "Match proportion", 
-     ylab = "Posterior relapse probability (based on genetic data alone)")
+     ylab = "Posterior probability (based on genetic data alone)")
 
-plot(x = matches_n, y = Uniform_Pv3Rs[,"I"], 
+plot(x = matches_p, y = Uniform_Pv3Rs[,"I"], # Use this plot for diagnosis
+     cex = matches_n/9,
      xlab = "Match proportion", 
-     ylab = "Posterior relapse probability (based on genetic data alone)")
+     ylab = "Posterior probability (based on genetic data alone)")
+# Annotate already identified cases
 
-# Something wrong? 
-suspect_epis <- names(which(matches_n == 0 & Uniform_Pv3Rs[,"L"] > 0.5))
+
+suspect_epis <- names(which(matches_p > 0.5 & Uniform_Pv3Rs[,"I"] > 0.5))
 suspect_pids <- unique(apply(do.call(rbind, strsplit(suspect_epis, split = "_"))[,1:2], 1, paste, collapse = "_"))
-
 plot_data(ys = ys_VHX_BPD[suspect_pids], fs = fs_VHX_BPD)
--- Code/match_counting.R
+++ Code/match_counting.R
@@ -8,21 +8,40 @@ match_counting <- function(x){
     if (!all(sapply(x, function(x_epi) all(names(x_epi) == markers)))) {
       stop("Not all episodes typed at the same markers")}
     
-    z <- sapply(1:(n_epi-1), function(i){
-      m <- sapply(1:n_markers, function(j) any(x[[i+1]][[j]] %in% x[[i]][[j]])) # get matches
-      m[is.na(x[[1]]) | is.na(x[[2]])] <- NA # replace NA matches with NA
-      p <- mean(m, na.rm = T) # proportion of matches
-      n <- sum(!is.na(m)) # number of markers with comparable data
-      return(rbind(p, n))
+    z <- sapply(2:n_epi, function(i){
+      
+      z_all <- sapply(1:(i-1), function(j) { # Compare to all previous
+        mat <- sapply(1:n_markers, function(m) any(x[[i]][[m]] %in% x[[j]][[m]])) # get matches
+        mat[is.na(x[[i]]) | is.na(x[[j]])] <- NA # replace NA matches with NA
+        p <- mean(mat, na.rm = T) # proportion matched
+        n <- sum(!is.na(mat)) # number of markers on which p is based
+        return(rbind(p, n))
+      })
+      
+      if (ncol(z_all) == 1 | all(is.na(z_all[1,]))) { # Only one or zero
+        z_max <- z_all[,1, drop = FALSE] # Return first
+      } else if (length(unique(z_all[1,])) == 1 & length(unique(z_all[2,])) == 1) { # All the same p and n
+        z_max <- z_all[,1, drop = FALSE] # Return first 
+      } else if (length(unique(z_all[1,])) == 1 & length(unique(z_all[2,])) > 1) { # All the same p 
+        z_max <- z_all[, which.max(z_all[2,]), drop = FALSE] # Take maximum of n
+      } else { # All different
+        z_max <- z_all[, which.max(z_all[1,]), drop = FALSE] # Take maximum of p
+      }
+    
+    return(z_max)
+  
     })
+    
     rownames(z) <- c("p", "n")
     if (!is.null(names(x))) colnames(z) <- names(x)[-1] # Return episode names if available
-  } else { z <- NA }
-  return(z)  
+    return(z)
+} else { z <- NA }
+return(z)  
 }
 
 x1 <- list(initial = list(m1 = NA, m2 = NA), 
-          recur = list(m1 = NA, m2 = NA))
+           recur = list(m1 = NA, m2 = NA), 
+           recur2 = list(m1 = NA, m2 = NA))
 
 x2 <- list(initial = list(m1 = c(1,2), m2 = c(1,4)), 
            recur = list(m1 = 1, m2 = 1))
