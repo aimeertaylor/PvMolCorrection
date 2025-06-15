a4dcdc79a04c08eb4df1f811a8c93e24c23e03ea
Aimee Taylor <aimee.r.taylor.85@gmail.com>
2025-06-15 23:22:16 +0200

    Update

--- Code/Compute_match_counting.R
+++ Code/Compute_match_counting.R
@@ -37,7 +37,18 @@ plot(x = matches_p, y = Uniform_Pv3Rs[,"I"], # Use this plot for diagnosis
      ylab = "Posterior probability (based on genetic data alone)")
 # Annotate already identified cases
 
+half_sibs <- c("VHX_39_2", "VHX_56_2", "VHX_91_2", "VHX_113_6", "VHX_329_4", "VHX_450_8", 
+               "VHX_489_4", "VHX_529_4", "VHX_532_4", "VHX_225_4")
 
-suspect_epis <- names(which(matches_p > 0.5 & Uniform_Pv3Rs[,"I"] > 0.5))
+text(x = matches_p[half_sibs], y = Uniform_Pv3Rs[half_sibs,"I"], labels = half_sibs)
+abline(h = 0.5, v = 4/9, lty = "dotted")
+abline(v = (0:9)/9, lty = "dotted", col = "red")
+
+suspect_epis <- names(which(matches_p >= 4/9 & Uniform_Pv3Rs[,"I"] > 0.5))
 suspect_pids <- unique(apply(do.call(rbind, strsplit(suspect_epis, split = "_"))[,1:2], 1, paste, collapse = "_"))
-plot_data(ys = ys_VHX_BPD[suspect_pids], fs = fs_VHX_BPD)
+for(suspect_pid in suspect_pids) {
+  plot_data(ys = ys_VHX_BPD[suspect_pid], fs = fs_VHX_BPD)
+  mtext(paste(suspect_epis[grepl(suspect_pid, suspect_epis)], 
+        collapse = " "))
+}
+
