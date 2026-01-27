9a04faa9d87bf5312b44913d5d4e245811c3ff9b
aimeertaylor <aimee.r.taylor.85@gmail.com>
2026-01-27 11:43:51 +0100

    Annotate plot of with Pv3Rs probs

--- Code/Analysable_data.R
+++ Code/Analysable_data.R
@@ -61,13 +61,38 @@ write.csv2(tab_MOIs[tab_MOIs$Total > 5 | tab_MOIs$Typed.episode.count > 3, ],
 
 # Plot all paired data
 if(Figs) png("../Figures/data_paired.png", width = 7, height = 7, units = "in", res = 300)
-Pv3Rs::plot_data(ys = ys_VHX_BPD[names(which(typd_epi_count > 1))], fs = fs_VHX_BPD)
+Pv3Rs::plot_data(ys = ys_VHX_BPD[names(which(typd_epi_count > 1))], 
+                 fs = fs_VHX_BPD, marker.annotate = F, person.vert = T,
+                 mar = c(2,2.8,1,2.8)) # Margin around main plot)
 if(Figs) dev.off()
 
 # Plot data for pids with a recurrence whose recurrent state probability could
-# not be estimated using the prototype
+# not be estimated using the prototype; annotate with probabilities given uniform prior
+pids <- c("VHX_239","VHX_461","VHX_39","VHX_52","VHX_583","VHX_33")
+load("../RData/results_Pv3Rs.RData")
+marg <- sapply(pids, function(pid) {
+  if(is.na(Uniform_joint[pid])) {
+    Uniform_pairwise[[pid]]
+  } else {
+    Uniform_joint[[pid]][["marg"]]
+  }
+})
+state_names <- c("Recrud.", "Relapse", "Reinf.")
+episodes <- unlist(sapply(marg, function(x) c(1, as.numeric(rownames(x)))))
+probs <- unlist(sapply(marg, function(x) apply(x, 1, max))) # Get max marginal estimates 
+states <- unlist(sapply(marg, function(x) apply(x, 1, function(z) state_names [which.max(z)])))
+main_mar <- c(5,2.6,1,2.6) # Margin around main plot
+
+
 if(Figs) png("../Figures/data_unestimatable.png", width = 7, height = 7, units = "in", res = 300)
-Pv3Rs::plot_data(ys = Pv3Rs::ys_VHX_BPD[c( "VHX_239","VHX_461","VHX_39","VHX_52","VHX_583","VHX_33")], fs = fs_VHX_BPD)
+Pv3Rs::plot_data(ys = Pv3Rs::ys_VHX_BPD[pids], mar = main_mar, 
+                 fs = fs_VHX_BPD, marker.annotate = F)
+par(fig = c(0,1,0,1), mar = main_mar) # Reset before text annotation (important)
+#box() # Toggle on and off to see plot limits for text() — not enacted in margin
+text(y = rep(0.21, length(probs)), adj = 1, # right justify 
+     x = seq(0.025, 0.98, length.out = length(episodes))[-which(episodes == 1)], 
+     labels = paste0(states, " ", 100*round(probs,2), "%"), 
+     cex = 0.7, srt = 90)
 if(Figs) dev.off()
 
 
