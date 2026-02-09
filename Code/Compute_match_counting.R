f1cb721261a35cb5fc16b000978df924ee0109de
aimeertaylor <aimee.r.taylor.85@gmail.com>
2026-02-09 14:42:26 +0100

    Add dcifer

--- Code/Compute_match_counting.R
+++ Code/Compute_match_counting.R
@@ -1,55 +1,94 @@
 library(Pv3Rs)
 source("match_counting.R")
+source("genetic_proximity.R")
+load("../RData/marg_results_Pv3Rs.RData") # Load posterior probabilities
 
 # Compute number of episodes per participant 
-n_epi_per_pid <- sapply(ys_VHX_BPD, function(x) length(x))
+n_epi_per_pid <- sapply(ys_VHX_BPD, length)
 
-# Compute matches for  paired data 
-matches_n_p <- sapply(ys_VHX_BPD[n_epi_per_pid > 1], match_counting)
-matches_p <- unlist(sapply(matches_n_p, function(x) x["p",]))
-matches_n <- unlist(sapply(matches_n_p, function(x) x["n",]))
+# Compute matches for pids with one or more recurrences 
+matches <- sapply(ys_VHX_BPD[n_epi_per_pid > 1], match_counting)
 
-# I first did names(matches_n) <- gsub(".", "_", names(matches_n), fixed = T)
-# Need following because otherwise pids with one episode miss "_2" e.g. BPD_103
-names(matches_p) <- names(matches_n) <- unlist(sapply(names(matches_n_p), function(n) {
-  paste(n, colnames(matches_n_p[[n]]), sep = "_")
+# Compute genetic proximity for pids with one or more recurrences
+proxies_one <- sapply(ys_VHX_BPD[n_epi_per_pid > 1], genetic_proximity, type = "rhat_one")
+proxies_avg <- sapply(ys_VHX_BPD[n_epi_per_pid > 1], genetic_proximity, type = "rhat_avg")
+
+# Extract values into vector form
+matches_p <- unlist(sapply(matches, function(x) x["p",])) # match proportion
+rhats_one <- unlist(sapply(proxies_one, function(x) x["rhat",])) # rhat
+rhats_avg <- unlist(sapply(proxies_avg, function(x) x["rhat",])) # rhat
+
+matches_n <- unlist(sapply(matches, function(x) x["n",])) # match marker count
+rhats_one_n <- unlist(sapply(proxies_one, function(x) x["nmarks",])) # rhat marker count
+rhats_avg_n <- unlist(sapply(proxies_avg, function(x) x["nmarks",])) # rhat marker count
+
+
+# Name extracted values; n.b., gsub(".", "_") does not work because pids with
+# one recurrence miss "_2" e.g. BPD_103
+names(matches_p) <- names(matches_n) <- unlist(sapply(names(matches), function(pid) {
+  paste(pid, colnames(matches_n_p[[pid]]), sep = "_") 
+}))
+names(rhats_one) <- names(rhats_one_n) <- unlist(sapply(names(proxies_one), function(pid) {
+  paste(pid, colnames(proxies_one[[pid]]), sep = "_") 
+}))
+names(rhats_avg) <- names(rhats_avg_n) <- unlist(sapply(names(proxies_avg), function(pid) {
+  paste(pid, colnames(proxies_avg[[pid]]), sep = "_") 
 }))
 
-# Load posterior probabilities
-load("../RData/marg_results_Pv3Rs.RData")
 
-if (!all(names(matches_n) == row.names(Uniform_Pv3Rs))) stop()
+if (!all(names(matches_p) == row.names(Uniform_Pv3Rs))) stop()
+if (!all(names(rhats_one) == row.names(Uniform_Pv3Rs))) stop()
+if (!all(names(rhats_avg) == row.names(Uniform_Pv3Rs))) stop()
 
-# Plot match proportion against 
-plot(x = matches_p, y = Uniform_Pv3Rs[,"L"], 
-     cex = matches_n/9,
-     xlab = "Match proportion", 
-     ylab = "Posterior relapse probability (based on genetic data alone)")
-
-plot(x = matches_p, y = Uniform_Pv3Rs[,"C"], 
-     cex = matches_n/9,
-     xlab = "Match proportion", 
-     ylab = "Posterior recrudescence probability (based on genetic data alone)")
-
-plot(x = matches_p, y = Uniform_Pv3Rs[,"I"], # Use this plot for diagnosis
-     cex = matches_n/9,
-     xlab = "Match proportion", 
-     ylab = "Posterior reinfection probability (based on genetic data alone)")
-# Annotate already identified cases
-
-half_sibs <- c("VHX_39_2", "VHX_56_2", "VHX_91_2", "VHX_113_6", "VHX_329_4", "VHX_450_8", 
+half_sibs <- c("VHX_39_2", "VHX_56_2", "VHX_91_2", "VHX_113_6", "VHX_450_8", 
                "VHX_489_4", "VHX_529_4", "VHX_532_4", "VHX_225_4")
 
-text(x = matches_p[half_sibs], y = Uniform_Pv3Rs[half_sibs,"I"], labels = half_sibs)
-abline(h = 0.5, v = 4/9, lty = "dotted")
-abline(v = (0:9)/9, lty = "dotted", col = "red")
+pdf("Pv3Rs vs distance.pdf")
+par(mfrow = c(2,2), pty = "s", height = 12, width = 12)
 
-suspect_epis <- names(which(matches_p >= 4/9 & Uniform_Pv3Rs[,"I"] > 0.5))
-suspect_pids <- unique(apply(do.call(rbind, strsplit(suspect_epis, split = "_"))[,1:2], 1, paste, collapse = "_"))
-for(suspect_pid in suspect_pids) {
-  plot_data(ys = ys_VHX_BPD[suspect_pid], fs = fs_VHX_BPD)
-  mtext(paste(suspect_epis[grepl(suspect_pid, suspect_epis)], 
-        collapse = " "))
-}
+# # Plot match proportion 
+plot(x = matches_p, y = 1-Uniform_Pv3Rs[,"I"],
+     cex = matches_n/9, pch = 19, bty = "n",
+     xlab = "genetic proximity: match proportion",
+     ylab = "Posterior relapse plus recrudescence probability (uniform prior on states)")
+text(x = matches_p[half_sibs], y = 1-Uniform_Pv3Rs[half_sibs,"I"],
+     labels = half_sibs, cex = 0.5, pos = 3)
+abline(h = 0.5, v = 4/9, lty = "dashed")
+abline(v = (0:9)/9, lty = "dotted", col = "grey")
 
-plot_data(ys = ys_VHX_BPD[suspect_pids], fs = fs_VHX_BPD)
+# Plot genetic proximity assuming one related genotype
+plot(x = rhats_one, y = 1-Uniform_Pv3Rs[,"I"], 
+     cex = rhats_one_n/9, pch = 19, bty = "n", 
+     xlab = "genetic proximity: relatedness assuming one related genotype", 
+     ylab = "Posterior relapse plus recrudescence probability (uniform prior on states)")
+text(x = rhats_one[half_sibs], y = 1-Uniform_Pv3Rs[half_sibs,"I"], 
+     labels = half_sibs, cex = 0.5, pos = 3)
+abline(h = 0.5, v = 0.5, lty = "dashed")
+# Add additional suspect 
+suspect_epi_one <- names(which(rhats_one >= 0.5 & Uniform_Pv3Rs[,"I"] > 0.5))
+extra <- suspect_epi_one[!suspect_epi_one %in% half_sibs]
+text(x = rhats_one[extra], y = 1-Uniform_Pv3Rs[extra,"I"], 
+     labels = extra, cex = 0.5, pos = 3, col = "red")
+
+
+# Plot genetic proximity: average relatedness
+plot(x = rhats_avg, y = 1-Uniform_Pv3Rs[,"I"], 
+     cex = rhats_avg_n/9, pch = 19, bty = "n", 
+     xlab = "genetic proximity: relatedness averaged over genotypes", 
+     ylab = "Posterior relapse plus recrudescence probability (uniform prior on states)")
+text(x = rhats_avg[half_sibs], y = 1-Uniform_Pv3Rs[half_sibs,"I"], 
+     labels = half_sibs, cex = 0.5, pos = 3)
+abline(h = 0.5, v = 0.5, lty = "dashed")
+# Add additional suspect 
+suspect_epi_avg <- names(which(rhats_avg >= 0.5 & Uniform_Pv3Rs[,"I"] > 0.5))
+extra <- suspect_epi_avg[!suspect_epi_avg %in% half_sibs]
+text(x = rhats_avg[extra], y = 1-Uniform_Pv3Rs[extra,"I"], 
+     labels = extra, cex = 0.5, pos = 3, col = "red")
+dev.off()
+
+
+# Inspect data 
+all(suspect_epi_avg %in% suspect_epi_one)
+suspect_pids_one <- unique(apply(do.call(rbind, strsplit(suspect_epi_one, split = "_"))[,1:2], 1, paste, collapse = "_"))
+plot_data(ys = ys_VHX_BPD[suspect_pids_one], fs = fs_VHX_BPD)
+
---
+++ Code/genetic_proximity.R
@@ -0,0 +1,129 @@
+################################################################################
+# Functions required to compute genetic proximity with dcifer
+#
+# From vignetteDcifer: If afreq contains “extra” alleles that are not listed in
+# dsmp, these alleles are added to dsmp. If dsmp has alleles not included in
+# afreq or listed with a frequency of 0, they are assigned a small probability
+# (optional minfreq argument) with subsequent renormalization.
+################################################################################
+
+# List marker and per-marker allele names 
+markers <- names(alleles_per_marker)
+alleles_per_marker <- sapply(fs_VHX_BPD, names)
+
+# Function to convert per-episode data formatted for pv3Rs (categorical lists
+# per marker) into dcifer format (binary list across all alleles per marker)
+pv3rs_to_dcifer <- function(y_epi, alleles_per_marker){
+  z <- sapply(markers, function(m) {
+    x <- rep(0, length(alleles_per_marker[[m]])) # construct binary vector
+    names(x) <- alleles_per_marker[[m]] # name binary vector with alleles
+    if (m %in% names(y_epi)) { # If m listed for y_epi
+      alleles <- y_epi[[m]] # extract alleles (NAs possibly included)
+      if (!all(is.na(alleles))) { # If m listed and successfully typed
+        x[as.character(alleles[!is.na(alleles)])] <- 1 # add one for non-na detected alleles
+      }}
+    return(x)
+  })
+  return(z)
+}
+
+# Function returns for each recurrence maximum genetic proximity using dcifer
+# relative to all previous episodes per participants
+genetic_proximity <- function(y, type = "rhat_one"){ # per participant
+  
+  n_epi <- length(y) # if at least one recurrence
+  
+  if(n_epi > 1) { 
+    max_rhat <- sapply(2:n_epi, function(i){ # max_rhat with max_nmarks and episode name
+      prev_rhats_nmarks <- sapply(1:(i-1), function(j) { # compare to all previous
+        # extract episode name (some ys have data on non-sequential episodes, e.g., VHX_419)
+        episode <- names(y)[j]
+        # extract paired episode data in dcifer format: 
+        pair <- sapply(y[c(i,j)], pv3rs_to_dcifer, alleles_per_marker, simplify = F) 
+        # compute number of markers with comparible data for pair ij
+        nmarks <- sum(apply(sapply(pair, function(epi) sapply(epi, sum)) > 0, 1, all))
+        
+        if(nmarks == 0) {
+          rhat = NA
+        } else {
+          cois <- determine_MOIs(y[c(i,j)]) # Use same MOIs as Pv3Rs 
+          afreq <- fs_VHX_BPD # Use same allele frequencies as Pv3Rs
+          if (type == "rhat_one") { # Assuming at most one genotype related 
+            rhat <- dcifer::ibdPair(pair, cois, afreq, M = 1)            
+          } else if (type == "rhat_avg") { # Quick approximate average relatedness
+            rhat <- unique(dcifer::ibdEstM(pair, cois, afreq, equalr = TRUE)) 
+          }
+        }
+        return(list(prev_epi = episode, rhat = rhat, nmarks = nmarks))
+      })
+      
+      # Extract maximum rhat; don't use which.max since possibly multiple
+      # comparisons with max value in which case we want the one with largest
+      # nmarks, not first (the return value which.max)
+      rhat_max <- max(unlist(prev_rhats_nmarks["rhat", ]), na.rm = T) 
+      rhat_max_i <- which(unlist(prev_rhats_nmarks["rhat", ]) == rhat_max)
+      
+      if (length(rhat_max_i) > 1) { # If multiple rhat_max values
+        # If among multiple rhat_max equally high nmarks, return first using which.max: 
+        nmarks_max_i <- which.max(unlist(prev_rhats_nmarks["nmarks", rhat_max_i])) 
+        return(prev_rhats_nmarks[, rhat_max_i[nmarks_max_i]])
+      } else {
+        return(prev_rhats_nmarks[, rhat_max_i])
+      }
+      
+    })
+    colnames(max_rhat) <- names(y)[2:n_epi]
+    return(max_rhat)
+  } else {
+    return(NA)
+  }
+}
+
+#===============================================================================
+# Sandbox: testing some dcifer calculations on VHX BPD
+#===============================================================================
+# pid <- "VHX_532"
+# y <- ys_VHX_BPD[[pid]]
+# plot_data(ys_VHX_BPD[pid], fs = fs_VHX_BPD, marker.annotate = F)
+# i <- 5; j = 1
+# pair <- sapply(y[c(i,j)], pv3rs_to_dcifer, alleles_per_marker, simplify = F)
+# cois <- determine_MOIs(y[c(i,j)])
+# revals <- mapply(generateReval, 1:5, nr = c(1e3, 1e2, 32, 16, 12))
+# # Compute r values for each genotype pair up to max(cois) assuming pairs are differentially related
+# rdiff <- ibdEstM(pair, cois, afreq = fs_VHX_BPD, Mmax = max(cois), equalr = FALSE, reval = revals) # slow
+# # Compute r values for each genotype pair up to max(cois) assuming pairs are equally related
+# rsame <- ibdEstM(pair, cois, afreq = fs_VHX_BPD, equalr = TRUE) # quick
+# # Average of different r values is close to the repeated value when genotypes are considered equal: 
+# mean(rdiff); rsame[1] 
+
+#===============================================================================
+# Close correlation between average relatedness and approximate average
+# relatedness is illustrated in code copied from vignetteDcifer:
+#===============================================================================
+# afile <- system.file("extdata", "MozAfreq.csv", package = "dcifer")
+# afreq <- readAfreq(afile, lvar = "locus", avar = "allele", fvar = "freq")
+# meta <- unique(read.csv(sfile)[c("sampleID", "province")])
+# meta <- meta[match(names(dsmp), meta$sampleID), ] # order samples as in dsmp
+# provinces <- c("Maputo", "Inhambane")
+# nsite <- table(meta$province)[provinces]
+# ord <- order(factor(meta$province, levels = provinces))
+# dsmp <- dsmp[ord]
+# coi <- coi[ord]
+# dres <- ibdDat(dsmp, coi, afreq, pval = TRUE, confint = TRUE, rnull = 0, alpha = 0.05, nr = 1e3) # Assumes M = 1
+# alpha <- 0.05 # significance level
+# sig <- dres[, , "p_value"] <= alpha
+# isig <- which(sig, arr.ind = TRUE)
+# nsig <- nrow(isig)
+# sig1 <- sig2 <- vector("list", nsig)
+# for (i in 1:nsig) {
+#   sig1[[i]] <- ibdEstM(dsmp[isig[i, ]], coi[isig[i, ]], afreq, Mmax = 5,
+#                        equalr = FALSE, reval = revals) 
+# }
+# for (i in 1:nsig) {
+#   sig2[[i]] <- ibdEstM(dsmp[isig[i, ]], coi[isig[i, ]], afreq, equalr = TRUE)
+# }
+# M1 <- sapply(sig1, function(r) sum(r > 0))
+# M2 <- sapply(sig2, length)
+# rtotal1 <- sapply(sig1, sum)
+# rtotal2 <- sapply(sig2, sum)
+
