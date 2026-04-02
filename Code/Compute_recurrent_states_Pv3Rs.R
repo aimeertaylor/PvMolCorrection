82fc94838ae2a93502b8fe96e15802cedca05ba7
Aimee Taylor <aimee.r.taylor.85@gmail.com>
2026-04-02 10:26:19 +0200

    Re-run results

--- Code/Compute_recurrent_states_Pv3Rs.R
+++ Code/Compute_recurrent_states_Pv3Rs.R
@@ -62,8 +62,8 @@ for(pid in paired_data_pids) {
     TimeToEvent_pairwise[[pid]] <- compute_posterior_approxjoint(y, fs_VHX_BPD, prior = prior_per_patient)
     Uniform_pairwise[[pid]] <- compute_posterior_approxjoint(y, fs_VHX_BPD)    
   } else { 
-    TimeToEvent_joint[[pid]] <- NA #compute_posterior(y, fs_VHX_BPD, prior = prior_per_patient)
-    Uniform_joint[[pid]] <- NA #compute_posterior(y, fs_VHX_BPD)
+    TimeToEvent_joint[[pid]] <- compute_posterior(y, fs_VHX_BPD, prior = prior_per_patient)
+    Uniform_joint[[pid]] <- compute_posterior(y, fs_VHX_BPD)
     TimeToEvent_pairwise[[pid]] <- compute_posterior_approxjoint(y, fs_VHX_BPD, prior = prior_per_patient)
     Uniform_pairwise[[pid]] <- compute_posterior_approxjoint(y, fs_VHX_BPD)
   }
